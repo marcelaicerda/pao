c5e27047890b96c414092ea4c7b196ac8320924d
marcela.i.cerda <marcela.i.cerda@gmail.com>
2024-04-16 14:20:21 -0300

    primer commit

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+// comentario de linea
+/*   
+    comentario de bloque
+*/
+console.log(3 + 3);
+console.log("Texto escrito en la consola")
+console.log("Comision:" + (24177 + 3))
+
+var edad= parseInt(  prompt("Ingrese su edad:") )  //  "999"
+edad=15 + 1 * 2
+var num=10 % 2  // 5 caramelos a cada chico sobre 0
+console.log(typeof(edad))    // numerico
+let nombre=prompt("Ingrese su nombre:")
+apellido=prompt("Ingrese su apellido")
+
+const PI=3.141592
+//PI=5 da error
+
+console.log("Edad:" + edad)
+console.log("Nombre:" + nombre)
+console.log("Apellido:" + apellido)
+
+
+
+
+document.write(`
+<header>
+<h1>Clase JS</h1>
+<nav>
+    <a href="">home</a>
+    <a href="">Registro</a>
+    <a href="">Sucursales</a>
+</nav>
+</header> `)
+/*
+document.write("<p>hola</p>")
+document.write("<h2>3</h2>")
+*/
